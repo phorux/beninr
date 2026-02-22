bd351cabc87bbee995492ac887d2cc214147fad8
@stangandaho <gandahostanmah@gmail.com>
2026-02-22 12:41:46 +0100

    agroeco_zones

--- R/agroeco_zones.R
+++ R/agroeco_zones.R
@@ -22,7 +22,12 @@
 #'   \[Data set\]. Distributed as part of the \pkg{<beninr>} R package.
 #'
 #' @examples
-#' \dontrun{
 #' data(agroeco_zones)
-#'}
+#'
+#' # Transform to projected system
+#' if (!requireNamespace("sf")) {
+#'   agroeco_zones <- agroeco_zones %>%
+#'     sf::st_transform(crs = "EPSG:32631")
+#' }
+#'
 "agroeco_zones"
--- data-raw/agroeco_zone_data.R
+++ data-raw/agroeco_zone_data.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 agroeco_zones <- read_sf("C:/Users/ganda/Downloads/Benin ZA/benin_za.shp") %>%
   st_set_crs(value = 4326) %>%
-  st_transform(crs = "EPSG:32631") %>%
+  st_transform(crs = "EPSG:32631") %>% # FOr accurate mesure calculation
   mutate(`AREA KM2` = round(as.numeric(st_area(.))/1e6, 2),
          `PERIMETER KM` = as.numeric(
            st_length(st_cast(x = ., to = "MULTILINESTRING")))/1e3,
@@ -11,6 +11,10 @@ agroeco_zones <- read_sf("C:/Users/ganda/Downloads/Benin ZA/benin_za.shp") %>%
          ) %>%
   relocate(geometry, .after = `PERIMETER KM`)
 
-Encoding(sf::st_crs(agroeco_zones)$wkt) <- "UTF-8"
+# I realized EPSG:32631 cause ASCII warning during the package check.
+# So transform back to geographic system.
+agroeco_zones <- agroeco_zones %>%
+  st_transform(crs = 4326)
 
+#Encoding(sf::st_crs(agroeco_zones)$wkt) <- "UTF-8"
 usethis::use_data(agroeco_zones, overwrite = TRUE)
